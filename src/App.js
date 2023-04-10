62dbd64bb7d48b1f94dc9077cb4531e1835fb2b1
yuKalasnikova2 <pmalian@list.ru>
2023-04-10 22:32:29 +0700

    feat(App): fetch, useEffect for array pizz

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import { useState, useEffect } from 'react'
 import './App.css'
 import './scss/app.scss'
 import Header from './components/Header'
 import Categories from './components/Categories'
 import Sort from './components/Sort'
 import PizzaBlock from './components/PizzaBlock'
-import  pizzas  from './assets/pizzas.json'
+
 
 function App() {
+  const [items, setItems] = useState([])
+
+  useEffect(() => {
+    fetch('https://64340e691c5ed06c958de2ee.mockapi.io/items')
+      .then((res) => res.json())
+      .then((json) => setItems(json))
+  }, [])
+
   return (
     <div className="App">
       <div className="wrapper">
@@ -19,8 +28,9 @@ function App() {
             </div>
             <h2 className="content__title">Все пиццы</h2>
             <div className="content__items">
-              {pizzas.map((obj, index) => <PizzaBlock key={index} {...obj} />)}
-        
+              {items.map((obj, index) => (
+                <PizzaBlock key={index} {...obj} />
+              ))}
             </div>
           </div>
         </div>
