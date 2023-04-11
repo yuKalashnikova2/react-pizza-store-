63feb4d1fa406b3a9d30e0c4c91fd7dbc66d6e75
yuKalasnikova2 <pmalian@list.ru>
2023-04-11 16:40:59 +0700

    feat(styles): add NotFoundBlock.module.scss

--- src/components/NotFoundBlock/NotFoundBlock.jsx
+++ src/components/NotFoundBlock/NotFoundBlock.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import styles from './NotFoundBlock.module.scss'
 
 const NotFoundBlock = () => {
-  return <h1>
-    <span>😔</span>
-    <br />
-    Ничего не найдено </h1>
+  return (
+    <div className={styles.root}>
+      <h1>
+        <span>😔</span>
+        <br />
+        Ничего не найдено
+      </h1>
+      <p className={styles.description}>
+        Такой страницы не существует в нашем интернет-магазине
+      </p>
+    </div>
+  )
 }
 
 export default NotFoundBlock
