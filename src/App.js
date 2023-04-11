f87586249c9e1abed5bf7e374e78b2f85d3f67d8
yuKalasnikova2 <pmalian@list.ru>
2023-04-11 14:27:54 +0700

    feat(App): add isLoading with Skeleton

--- src/App.js
+++ src/App.js
@@ -5,15 +5,19 @@ import Header from './components/Header'
 import Categories from './components/Categories'
 import Sort from './components/Sort'
 import PizzaBlock from './components/PizzaBlock/PizzaBlock'
-
+import Skeleton from './components/PizzaBlock/Skeleton'
 
 function App() {
   const [items, setItems] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
     fetch('https://64340e691c5ed06c958de2ee.mockapi.io/items')
       .then((res) => res.json())
-      .then((json) => setItems(json))
+      .then((json) => {
+        setItems(json)
+        setIsLoading(false)
+      })
   }, [])
 
   return (
@@ -28,9 +32,11 @@ function App() {
             </div>
             <h2 className="content__title">Все пиццы</h2>
             <div className="content__items">
-              {items.map((obj, index) => (
-                <PizzaBlock key={index} {...obj} />
-              ))}
+              {isLoading
+                ? [...new Array(9)].map((_, index) => <Skeleton key={index} />)
+                : items.map((obj, index) => (
+                    <PizzaBlock key={index} {...obj} />
+                  ))}
             </div>
           </div>
         </div>
