dfd14ff90a1447f86e90c4bb0b10d038fc329914
yuKalasnikova2 <pmalian@list.ru>
2023-04-19 11:31:48 +0700

    refactor(sliceCart): optimization add same pizzas with help COUNT

--- src/redux/slices/sliceCart.js
+++ src/redux/slices/sliceCart.js
@@ -2,28 +2,39 @@ import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = {
   totalPrice: 0,
-  items: []
+  items: [],
 }
 
 export const sliceCart = createSlice({
   name: 'cart',
   initialState,
   reducers: {
+    // addItem(state, action) {
+    //   state.items.push(action.payload)
+    //   state.totalPrice = state.items.reduce((sum, obj) => {
+    //     return obj.price + sum;
+    //   }, 0)
+    // },
     addItem(state, action) {
-      state.items.push(action.payload)
+      const findItem = state.items.find((obj) => obj.id === action.payload.id)
+      if (findItem) {
+        findItem.count++
+      } else {
+        state.items.push({ ...action.payload, count: 1 })
+      }
       state.totalPrice = state.items.reduce((sum, obj) => {
-        return obj.price + sum;
+        return obj.price + sum
       }, 0)
     },
     removeItem(state, action) {
-       state.items =  state.items.filter(obj => obj.id !== action.payload)
+      state.items = state.items.filter((obj) => obj.id !== action.payload)
     },
     clearItems(state, action) {
-        state.items = []
-    }
+      state.items = []
+    },
   },
 })
 
-export const { addItem, removeItem,clearItems } = sliceCart.actions
+export const { addItem, removeItem, clearItems } = sliceCart.actions
 
 export default sliceCart.reducer
