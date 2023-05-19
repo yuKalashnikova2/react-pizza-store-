4b38b618a0ac63926fcc63c792ceb16901d0d865
yuKalasnikova2 <pmalian@list.ru>
2023-05-19 15:19:54 +0700

    refactor(CartEmpty): jsx -> tsx; add @type for images

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,10 @@ import { BrowserRouter } from 'react-router-dom'
 import { store } from './redux/store'
 import { Provider } from 'react-redux'
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+const rootElem = document.getElementById('root')
+
+if(rootElem) {
+  const root = ReactDOM.createRoot(rootElem)
 root.render(
   <BrowserRouter>
     <Provider store={store}>
@@ -13,3 +16,5 @@ root.render(
     </Provider>
   </BrowserRouter>
 )
+}
+
