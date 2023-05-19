6c906e795d905494918824f3a302b3116d2eb38e
yuKalasnikova2 <pmalian@list.ru>
2023-05-19 21:15:33 +0700

    refactor(Cart): jsx -> tsx

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,14 +1,14 @@
 import { Link } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import CartItem from '../components/CartItem'
-import { clearItems } from '../redux/slices/sliceCart'
+import { clearItems, selectCart } from '../redux/slices/sliceCart'
 import CartEmpty from '../components/CartEmpty'
 
-const Cart = () => {
+const Cart: React.FC = () => {
   const dispatch = useDispatch()
-  const { totalPrice, items } = useSelector((store) => store.cart)
+  const { totalPrice, items } = useSelector(selectCart)
 
-  const totalCount = items.reduce((sum, item) => sum + item.count, 0)
+  const totalCount = items.reduce((sum: number, item: any) => sum + item.count, 0)
 
   const onClickClearCart = () => {
     if (window.confirm('Очистить корзину?')) {
@@ -98,7 +98,7 @@ const Cart = () => {
           </div>
         </div>
         <div className="content__items">
-          {items.map((item) => (
+          {items.map((item: any) => (
             <CartItem key={item.id} {...item} />
           ))}
         </div>
